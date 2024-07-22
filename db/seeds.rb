4a9217c1df42dff9c9d9a4656cb9306d87590515
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-22 15:47:41 -0400

    Modify seeds code to test methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@ Role.create([
 ])
 
 Audition.create([
-  { actor: 'John Doe', location: 'New York', phone: 1234567890, hired: false, role_id: Role.find_by(character_name: 'Hamlet').id },
+  { actor: 'John Doe', location: 'New York', phone: 1234567890, hired: true, role_id: Role.find_by(character_name: 'Hamlet').id },
+  { actor: 'Jason Bourne', location: 'New York', phone: 9992345678, hired: true, role_id: Role.find_by(character_name: 'Hamlet').id },
   { actor: 'Jane Smith', location: 'Los Angeles', phone: 9876543210, hired: true, role_id: Role.find_by(character_name: 'Ophelia').id },
   { actor: 'Alice Johnson', location: 'Chicago', phone: 5556667777, hired: false, role_id: Role.find_by(character_name: 'Macbeth').id },
   { actor: 'Bob Brown', location: 'San Francisco', phone: 4445556666, hired: true, role_id: Role.find_by(character_name: 'Lady Macbeth').id }
